2ed60ac66db8f58c2552109577936fc49d888228
meih124 <meih0124@gmail.com>
2020-03-18 20:19:00 -0700

    Added "artwork" relational data

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from marshmallow_jsonapi.flask import Relationship
+from flask_rest_jsonapi import ResourceRelationship
 
 # Create a new Flask application
 app = Flask(__name__)
@@ -14,6 +16,15 @@ class Artist(db.Model):
 	name = db.Column(db.String)
 	birth_year = db.Column(db.Integer)
 	genre = db.Column(db.String)
+
+# Define the Artwork table
+class Artwork(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String)
+	artist_id = db.Column(db.Integer,
+		db.ForeignKey('artist.id'))
+	artist = db.relationship('Artist',
+		backref=db.backref('artworks'))
 
 # Create the table
 db.create_all()
@@ -35,8 +46,23 @@ class ArtistSchema(Schema):
 	name = fields.Str(required=True)
 	birth_year = fields.Integer(load_only=True)
 	genre = fields.Str()
+	artworks = Relationship(self_view = 'artist_artworks',
+		self_view_kwargs = {'id': '<id>'},
+		related_view = 'artwork_many',
+		many = True,
+		schema = 'ArtworkSchema',
+		type_ = 'artwork')
 
+class ArtworkSchema(Schema):
+	class Meta:
+		type_ = 'artwork'
+		self_view = 'artwork_one'
+		self_view_kwargs = {'id': '<id>'}
+		self_view_many = 'artwork_many'
 
+	id = fields.Integer()
+	title = fields.Str(required=True)
+	artist_id = fields.Integer(required=True)
 
 # Create resource managers and endpoints
 from flask_rest_jsonapi import Api, ResourceDetail, ResourceList
@@ -51,9 +77,27 @@ class ArtistOne(ResourceDetail):
 	data_layer = {'session': db.session,
 					'model': Artist}
 
+class ArtworkMany(ResourceList):
+	schema = ArtworkSchema
+	data_layer = {'session': db.session,
+					'model': Artwork}
+
+class ArtworkOne(ResourceDetail):
+	schema = ArtworkSchema
+	data_layer = {'session': db.session,
+					'model': Artwork}
+
+class ArtistArtwork(ResourceRelationship):
+	schema = ArtistSchema
+	data_layer = {'session': db.session,
+					'model': Artist}
+
 api = Api(app)
 api.route(ArtistMany, 'artist_many', '/artists')
 api.route(ArtistOne, 'artist_one', '/artists/<int:id>')
+api.route(ArtworkOne, 'artwork_one', '/artworks/<int:id>')
+api.route(ArtworkMany, 'artwork_many', '/artworks')
+api.route(ArtistArtwork, 'artist_artworks', '/artists/<int:id>/relationships/artworks')
 
 # main loop to run app in debug mode
 if __name__ == '__main__':
